db55b1e35ce5aaed6c32fd0056dae8b105c99043
Eltaweel <mohammedeltaweel40@gmail.com>
2023-08-26 22:12:48 +0300

    added year values

--- Value Slider/app.py
+++ Value Slider/app.py
@@ -5,21 +5,15 @@
 from dash import Dash, dcc, html, Input, Output, clientside_callback, callback
 
 app = Dash(__name__)
-
-ALLOWED_TYPES = (
-    "text", "number", "password", "email", "search",
-    "tel", "url", "range", "hidden",
-)
-
-
+year_values = [2017, 2018, 2019, 2020]
 app.layout = html.Div(className="range-input", children=[
         dcc.Input(
             id="my-slider",
             type="range",
-            min=0,
-            max=100,
-            value=0,
-            step=20,
+            min=min(year_values),
+            max=max(year_values),
+            value=year_values[0],
+            step=year_values[2] - year_values[1],
             autoFocus=True
         ),
         html.Div(className="value", children=html.Div(id="output-div-js")),
@@ -39,11 +33,11 @@ Output('output-div-js', 'children'), Input('my-slider', 'value'))
 
 # Uncomment to use the slider-value-dash div to show slider value instead of clientside callback
 # @callback(
-#     Output("slider-value", "children"),
+#     Output("slider-value-dash", "children"),
 #     Input("my-slider", "value")
 # )
 # def update_slider(value_of):
-    # return value_of
+#     return value_of
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Value Slider/assets/script.js
+++ Value Slider/assets/script.js
@@ -2,16 +2,22 @@ function sliderValShow(){
 let rangeInput = document.querySelector(".range-input input");
 let rangeValue = document.querySelector(".range-input .value div");
 
-let start = parseFloat(rangeInput.min);
-let end = parseFloat(rangeInput.max);
-let step = parseFloat(rangeInput.step);
+let start = parseInt(rangeInput.min);
+let end = parseInt(rangeInput.max);
+let step = parseInt(rangeInput.step);
+console.log(start);
+console.log(end);
+console.log(step);
 
 for(let i=start;i<=end;i+=step){
   rangeValue.innerHTML += '<div>'+i+'</div>';
 }
 
 rangeInput.addEventListener("input",function(){
-  let top = parseFloat(rangeInput.value)/step * -40;
+  let top = parseInt(rangeInput.value - start) * -40;
+
+
+  console.log(top);
   rangeValue.style.marginTop = top+"px";
 });
 }
