86741acf7e2b87214caef54d17f8b6b823c890c7
Eltaweel <mohammedeltaweel40@gmail.com>
2023-08-27 02:30:00 +0300

    added interactivity to the slider

--- Value Slider/assets/script.js
+++ Value Slider/assets/script.js
@@ -1,25 +1,49 @@
+// function sliderValShow(){
+// let rangeInput = document.querySelector(".range-input input");
+// let rangeValue = document.querySelector(".range-input .value div");
+
+// let start = parseInt(rangeInput.min);
+// let end = parseInt(rangeInput.max);
+// let step = parseInt(rangeInput.step);
+// console.log(start);
+// console.log(end);
+// console.log(step);
+
+// for(let i=start;i<=end;i+=step){
+//   rangeValue.innerHTML += '<div>'+i+'</div>';
+// }
+
+// rangeInput.addEventListener("input",function(){
+//   let top = parseInt(rangeInput.value - start) * -40;
+
+
+//   console.log(top);
+//   rangeValue.style.marginTop = top+"px";
+// });
+// }
+
+// Get the range input element
 function sliderValShow(){
-let rangeInput = document.querySelector(".range-input input");
-let rangeValue = document.querySelector(".range-input .value div");
+const rangeInput = document.querySelector('.range-input input');
+// Get the labels
+const labels = document.querySelectorAll('.range-labels-item');
 
-let start = parseInt(rangeInput.min);
-let end = parseInt(rangeInput.max);
-let step = parseInt(rangeInput.step);
-console.log(start);
-console.log(end);
-console.log(step);
+// Add a click event listener to the range input
+rangeInput.addEventListener('click', function () {
+    // Remove the "active" class from all labels
+    labels.forEach(label => {
+        label.classList.remove('active');
+    });
 
-for(let i=start;i<=end;i+=step){
-  rangeValue.innerHTML += '<div>'+i+'</div>';
-}
+    // Get the selected year value
+    const selectedYear = parseInt(rangeInput.value);
 
-rangeInput.addEventListener("input",function(){
-  let top = parseInt(rangeInput.value - start) * -40;
+    // Add the "active" class to the corresponding label
+    const correspondingLabel = document.getElementsByClassName(`item-${selectedYear}`)[0];
 
-
-  console.log(top);
-  rangeValue.style.marginTop = top+"px";
+    // console.log(correspondingLabel);
+    if (correspondingLabel) {
+        correspondingLabel.classList.add('active');
+    }
 });
 }
-
-
